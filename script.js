9e3ac6560e6aa3fc403612a415dd471cddcba38a
devAymane <aymanexiaomi07@gmail.com>
2026-05-22 10:19:37 +0100

    Ajout des fonctions API CRUD

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ let view = "home";
 let genre = "ALL";
 let search = "";
 
+// ================= API =================
+const api = {
+  get: async () => (await fetch(API)).json(),
+
+  getOne: async (id) =>
+    (await fetch(`${API}/${id}`)).json(),
+
+  add: async (data) =>
+    fetch(API, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    }),
+
+  update: async (id, data) =>
+    fetch(`${API}/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    }),
+
+  delete: async (id) =>
+    fetch(`${API}/${id}`, { method: "DELETE" })
+};
