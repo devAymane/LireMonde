eeafe5fb41097c8e502827739107f371d72592ea
devAymane <aymanexiaomi07@gmail.com>
2026-05-22 10:22:13 +0100

    Ajout du chargement des livres et des filtres

--- script.js
+++ script.js
@@ -35,3 +35,40 @@ const api = {
   delete: async (id) =>
     fetch(`${API}/${id}`, { method: "DELETE" })
 };
+
+
+// ================= LOAD =================
+async function loadBooks() {
+  books = await api.get();
+  render();
+  updateBadge();
+}
+
+function updateBadge() {
+  badge.textContent = books.filter(b => b.toRead).length;
+}
+
+// ================= FILTER =================
+function filteredBooks() {
+  let data = [...books];
+
+  if (view === "toread") {
+    data = data.filter(b => b.toRead);
+  }
+
+  if (genre !== "ALL" && view === "home") {
+    data = data.filter(b => b.genre === genre);
+  }
+
+  if (search.trim()) {
+    const q = search.toLowerCase();
+
+    data = data.filter(b =>
+      b.title.toLowerCase().includes(q) ||
+      b.author.toLowerCase().includes(q) ||
+      b.genre.toLowerCase().includes(q)
+    );
+  }
+
+  return data;
+}
